869093f02fd96b36bc37eb5b1feeb84e7dbc7cec
Akshi2618 <e23157@eng.pdn.ac.lk>
2026-09-25 18:34:23 +0530

    Enhance security by adding role-based access control to Hospital and Inventory controllers

--- code/backend/src/main/java/com/bbms/backend/controller/InventoryController.java
+++ code/backend/src/main/java/com/bbms/backend/controller/InventoryController.java
@@ -5,10 +5,11 @@ import com.bbms.backend.Repository.InventoryRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping("/api/inventory")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class InventoryController {
 
     private final InventoryRepository repo;
@@ -17,13 +18,17 @@ public class InventoryController {
         this.repo = repo;
     }
 
-    // GET ALL
+    // ================= GET ALL =================
+    // ADMIN + LAB_STAFF + HOSPITAL_STAFF + RECEPTION_STAFF
+    @PreAuthorize("hasAnyRole('ADMIN', 'LAB_STAFF', 'HOSPITAL_STAFF', 'RECEPTION_STAFF')")
     @GetMapping
     public List<Inventory> getAll() {
         return repo.findAll();
     }
 
-    // FILTER BY BLOOD GROUP
+    // ================= FILTER BY BLOOD GROUP =================
+    // ADMIN + LAB_STAFF + HOSPITAL_STAFF + RECEPTION_STAFF
+    @PreAuthorize("hasAnyRole('ADMIN', 'LAB_STAFF', 'HOSPITAL_STAFF', 'RECEPTION_STAFF')")
     @GetMapping("/group/{group}")
     public List<Inventory> getByGroup(@PathVariable String group) {
         return repo.findByBloodGroup(group);
